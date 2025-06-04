2b81abf3b669ac9d53540e965def3a6232ae6d5d
naresh narayanadasu <nareshnrnds@gmail.com>
2025-06-04 14:40:33 +0530

    addded

---
+++ meals/views.py
@@ -0,0 +1,8 @@
+
+from rest_framework import viewsets
+from .models import Meal
+from .serializers import MealSerializer
+
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
--- tiffinbox/meals/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ChefProfile, Meal, Order
-admin.site.register(ChefProfile)
-admin.site.register(Meal)
-admin.site.register(Order)
