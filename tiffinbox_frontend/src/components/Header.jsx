848d9ab3acc699c2aef05c8dddc5760479ee63b2
naresh narayanadasu <nareshnrnds@gmail.com>
2025-06-04 15:11:13 +0530

    updated front end

---
+++ tiffinbox_frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-green-600 text-white p-4 text-center text-xl font-bold">
+      TiffinBox - Daily Meals for You 🍱
+    </header>
+  );
+}
